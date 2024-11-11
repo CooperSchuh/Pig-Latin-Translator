2e0d97e80dde50503cbceb6c173bbf95df80ce7c
CooperSchuh <cs2322@pleasantonusd.net>
2024-11-11 23:22:06 +0000

    revised the word translator and added the line translator

--- src/PigLatinTranslator.java
+++ src/PigLatinTranslator.java
@@ -15,14 +15,43 @@ public class PigLatinTranslator
   public static String translate(String input)
   {
     // System.out.println("Translate String: '" + input + "'");
+    int i = 0;
+    String characters = "";
+    String line = "";
+    int n = input.length();
+    String trueOrFalse = "";
+    String letters = "abcdefghijklmnopqrstuvwxyz";
+    while (i<n){
+        if (letters.indexOf(input.charAt(i)) != -1){
+          trueOrFalse += "T";
+        }
+        else {
+          trueOrFalse += "F";
+          i++;
+        }
+    }
+    while (trueOrFalse.length()>0){
+      while(trueOrFalse.charAt(0) == 'T'){
+        characters += input.substring(0, 1);
+        trueOrFalse = trueOrFalse.substring(1);
+        input = input.substring(1);
+      }
+      line += translateWord(characters);
+      characters = "";
+      while(trueOrFalse.charAt(0) == 'F'){
+        line += input.substring(0, 1);
+        trueOrFalse = trueOrFalse.substring(1);
+        input = input.substring(1);
+      }
+    }
+    return line;
+
 
     // Replace this code to translate a string input.
     // The input to this function could be any English string. 
     // A sentence, paragraph, or a single word. 
     // It should call translateWord at least once.
-    String result = translateWord(input);
-
-    return result;
+    
   }
 
   private static String translateWord(String input)
@@ -30,11 +59,17 @@ public class PigLatinTranslator
     // System.out.println("translateWord: '" + input + "'");
 
     // Replace this code to correctly translate a single word.
+    String word = input;
+    String vowels = "aeiou";
+    word = word.substring(0,1).toLowerCase() + word.substring(1);
+    while (vowels.indexOf(word.charAt(0)) == -1){
+      word = word.substring(1)+ word.substring(0,1);
+    }
+    word += "ay";
+    //word = word.substring(0,1).toUpperCase() + word.substring(1);
 
-    String result = input;
-    if (result.indexOf("a")==0 || result.indexOf("e")==0 || result.indexOf("o")==0 || result.indexOf("i")==0 || result.indexOf("u")==0)
     
-    return result;
+    return word;
   }
 
   // Add additonal private methods here.
